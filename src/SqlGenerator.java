398b86ad0ec2a9fecc69e3146e732deb57bfc8e6
city gone <cityvoy@gmail.com>
2026-04-23 10:45:48 -0400

    Initial commit

--- src/SqlGenerator.java
+++ src/SqlGenerator.java
@@ -1,6 +1,10 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
 public class SqlGenerator {
 
-    public static void rooms(){
+    public static void Rooms(){
 
         for (int floor = 0; floor <= 8;floor++){
 
@@ -24,17 +28,57 @@ public class SqlGenerator {
 
 
                 for (int room = 1; room <= 20; room++){
+                    System.out.print("INSERT INTO Rooms (location) VALUES ('");
                     if (floor == 0){
-                        System.out.println("B" + realWing + room);
+                        System.out.print("B" + realWing + room +"')");
                     }else{
-                        System.out.println(floor + realWing + room);
+                        System.out.print(+ floor + realWing + room +"')");
                     }
+                    System.out.print(";");
+                    System.out.println();
 
                 }
 
             }
 
         }
+
+    }
+    public static void Departments() throws FileNotFoundException {
+        Scanner scan = new Scanner(new File("src/teachers"));
+        String[] depts = {"Biological Sciences",
+                "Chemistry",
+                "Computer Science",
+                "English",
+                "Health & PE",
+                "Mathematics",
+                "Physics",
+                "Social Studies",
+                "Special Education",
+                "Visual & Performing Arts",
+                "World Languages & ENL"
+        };
+
+        for (int x = 0; x < depts.length; x++){
+            String dept = depts[x];
+            System.out.println("INSERT INTO Departments VALUES ("+x+",'" + dept +"');");
+
+            while(scan.hasNextLine()){
+                String name = scan.nextLine();
+                if (x+1 < depts.length && name.equals(depts[x+1])){
+                    break;
+                }
+                String[] firstLast = name.split(" ",2);
+                System.out.println("INSERT INTO Teachers (first_name,last_name,departmentID) VALUES('"+firstLast[0]+"','"+firstLast[1]+"',"+x+")");
+
+            }
+
+
+        }
+
+    }
+
+    public static void Teachers(){
 
     }
 
