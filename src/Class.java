57814ad1b97b5ff6d83eed386d9ef0a19547366e
city gone <cityvoy@gmail.com>
2026-04-29 10:29:10 -0400

    haw

--- src/Class.java
+++ src/Class.java
@@ -3,7 +3,7 @@ import java.util.ArrayList;
 public class Class {
     private String name;
     private int type;
-    private ArrayList<Student> Students;
+//    private ArrayList<Student> Students;
     private ArrayList<Assignment> Assignments;
     private int id;
     private Room room;
@@ -13,10 +13,10 @@ public class Class {
         this.type = type;
         System.out.println("echo \"INSERT INTO Courses(name, type, id) VALUES(" + name + "," + type + "," + id + "(\"");
     }
-
-    public void addStudents (Student a){
-        Students.add(a);
-    }
+//
+//    public void addStudents (Student a){
+//        Students.add(a);
+//    }
     public void makeAssignments (){
         /// this SHOULD ONLY RUN ONCE
         for (int i = 0; i < 12; i++) {
@@ -32,15 +32,15 @@ public class Class {
                     " (" + Assignments.get(i).getName() + "," +Assignments.get(i).getType() +"," + id + ")\"");
         }
     }
-    public void giveGrade(){
-        for (int i = 0; i < Students.size(); i++) {
-            int studentID = Students.get(i).getId();
-            for (int j = 0; j < Assignments.size(); j++) {
-                int AssignmentID = Assignments.get(j).getId();
-                System.out.println("echo \" INSERT INTO Grades(Grade, studentID,assignmentID) VALUES("
-                        + (Math.random()* 25 + 75) + ","  + studentID + "," + "" + AssignmentID + ")\"");
-
-            }
-        }
-    }
+//    public void giveGrade(){
+//        for (int i = 0; i < Students.size(); i++) {
+//            int studentID = Students.get(i).getId();
+//            for (int j = 0; j < Assignments.size(); j++) {
+//                int AssignmentID = Assignments.get(j).getId();
+//                System.out.println("echo \" INSERT INTO Grades(Grade, studentID,assignmentID) VALUES("
+//                        + (Math.random()* 25 + 75) + ","  + studentID + "," + "" + AssignmentID + ")\"");
+//
+//            }
+//        }
+//    }
 }
