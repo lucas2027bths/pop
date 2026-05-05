965b78baef15cc932c327bfc3bd538d062ebd9d6
a <@>
2026-05-05 09:49:55 -0400

    Roster update

--- src/tester.java
+++ src/tester.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 public class tester {
     public static void main(String[] args) throws FileNotFoundException {
         SqlGenerator sqlGenerator = new SqlGenerator();
+        sqlGenerator.ClassesInitializer();
         ArrayList<Roster> rosters = new ArrayList<>();
         for (Student student : SqlGenerator.StudentList) {
             Roster roster = new Roster(student);
