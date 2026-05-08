072fe597943d3981eeeb2266aa2a7284e6e8ff70
lucas home pc <citboy@gmail.com>
2026-05-08 18:17:19 -0400

    packet to big

--- run.sh
+++ run.sh
@@ -3,4 +3,4 @@ cd src
 javac *.java
 touch ~/pop/populate.sql
 java Main > ~/pop/populate.sql
-mysql -h 127.0.0.1 -u "$USER" -p < ~/pop/populate.sql
+mysql  --max_allowed_packet=100M -h 127.0.0.1 -u "$USER" -p < ~/pop/populate.sql
