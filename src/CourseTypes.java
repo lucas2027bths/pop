aed0f72462401b9597ba9fde5fbeb6b64c449fc1
lucas home pc <citboy@gmail.com>
2026-05-08 18:06:19 -0400

    for some reason doesn't insert coursetypes

--- src/CourseTypes.java
+++ src/CourseTypes.java
@@ -1,5 +1,9 @@
 public class CourseTypes {
     public static String insertTypes (){
-        return "INSERT INTO CourseTypes Values(1,'Regents');INSERT INTO CourseTypes Values(2,'AP');INSERT INTO CourseTypes Values(3,'Elective');";
+        return """
+                INSERT INTO CourseTypes Values(1,'Regents');
+                INSERT INTO CourseTypes Values(2,'AP');
+                INSERT INTO CourseTypes Values(3,'Elective');
+                """;
     }
 }
