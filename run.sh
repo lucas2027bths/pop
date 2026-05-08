f15fdbe7f90a8bd228ff0ad85f5ff0dcf64ecb0b
miller <asd@gmail.com>
2026-05-08 10:15:54 -0400

    auto run

--- run.sh
+++ run.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 cd src
 javac *.java
-touch ../pop.sql
-java tester > ../pop.sql
+touch ~/pop/populate.sql
+java tester > ~/pop/populate.sql
+mysql -h 127.0.0.1 -u "$USER" -p < ~/pop/populate.sql
