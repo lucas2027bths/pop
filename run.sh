4da6903f8ac512b2e5d597e8d7f0b7434d128582
city gone <cityvoy@gmail.com>
2026-05-19 10:44:02 -0400

    fix class

--- run.sh
+++ run.sh
@@ -4,4 +4,6 @@ cd src
 javac *.java
 touch ~/pop/populate.sql
 java Main > ~/pop/populate.sql
-mysql --max_allowed_packet=100M -h 127.0.0.1 -u "$USER" -p < ~/pop/populate.sql
+mkdir ~/pop/split
+split -l 1000 -d ~/pop/populate.sql ~/pop/split/piece_
+ls | sort -V | while read file; do mysql --max_allowed_packet=100M -h 127.0.0.1 -u "$USER" -p"20795756" < "$file"; done
