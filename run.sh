33099955444ef2efabb8312faf3823f4a27fffbd
city gone <cityvoy@gmail.com>
2026-05-26 10:13:37 -0400

    fix shell script

--- run.sh
+++ run.sh
@@ -1,9 +1,23 @@
-
 #!/bin/bash
+POPULATIONSTATEMENTS=~/pop/populate.sql
+SPLITDIR=~/pop/splitFiles
 cd src
 javac *.java
-touch ~/pop/populate.sql
-java Main > ~/pop/populate.sql
-mkdir ~/pop/split
-split -l 1000 -d ~/pop/populate.sql ~/pop/split/piece_
-ls | sort -V | while read file; do mysql --max_allowed_packet=100M -h 127.0.0.1 -u "$USER" -p"20795756" < "$file"; done
+touch $POPULATIONSTATEMENTS
+java Main > $POPULATIONSTATEMENTS
+
+if [ -d "$SPLITDIR" ]; then
+        rm $SPLITDIR/*
+        rmdir $SPLITDIR
+fi
+
+mkdir $SPLITDIR
+
+split -l 8000 -d $POPULATIONSTATEMENTS $SPLITDIR/chunk_
+
+read -s -p "What is your sql password? " PASSWORD
+echo ""
+
+for file in $(ls "$SPLITDIR" | sort -V); do
+    mysql --max_allowed_packet=100M -h 127.0.0.1 -u "$USER" -p"$PASSWORD" -D "${USER}_db" < "$SPLITDIR/$file"
+done
